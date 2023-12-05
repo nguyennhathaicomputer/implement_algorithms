14f2978f5e990a458dce9c9eefcd199998d65acd
nguyennhathaicomputer <nguyennhathaigithub@gmail.com>
2023-12-05 13:06:35 +0700

    add line import pandas

--- KNN_sklearn.py
+++ KNN_sklearn.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
 iris = datasets.load_iris()
 X = iris.data
